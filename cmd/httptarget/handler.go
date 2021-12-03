7a7154a07307fede5bce9231a3669ca9db5fadb6
Gregory Brail <gregbrail@google.com>
2021-12-03 11:02:46 -0800

    Add a "database" to test target
    Now we can POST, PUT, DELETE, and GET to simulate common REST patterns.

--- cmd/httptarget/handler.go
+++ cmd/httptarget/handler.go
@@ -3,8 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
@@ -15,13 +19,23 @@ const (
 	kPattern     = "0123456789"
 )
 
-const helpMessage = `GET  /              : Print default message
-GET  /help          : Print this message
-POST /echo          : Echo back whatever you posted
-GET  /json          : Return JSON content. Use optional "size" parameter for extra data.
-GET  /json-trailers : Return JSON content with the trailer "x-test-target: Yes"
-GET  /data?size=xxx : Return arbitrary characters xxx bytes long
+const helpMessage = `GET  /                 : Print default message
+GET  /help             : Print this message
+POST /echo             : Echo back whatever you posted
+GET  /json             : Return JSON content.
+                         Use optional "size" parameter for extra data.
+GET  /json-trailers    : Return JSON content with the trailer 
+                         "x-test-target: Yes"
+GET  /data?size=xxx    : Return arbitrary characters xxx bytes long
+POST /database         : Store arbitrary JSON, looking for a field
+                         named "key" as the key
+GET  /database/{key}   : Return the record stored by POST to /database
+DELETE /database/{key} : Remove the record stored by POST to /database
+PUT /database/{key}    : Update the record stored by POST to /database,
+                         or 404 if not found
 `
+
+var jsonContent = regexp.MustCompile("^application/json(;.*)?$")
 
 type sampleJsonMessage struct {
 	Testing       int    `json:"testing"`
@@ -30,7 +44,13 @@ type sampleJsonMessage struct {
 	ExtraData     []byte `json:"extraData,omitempty"`
 }
 
+type databaseRecord struct {
+	Key string `json:"key"`
+	// The rest of the structure is opaque to us
+}
+
 func createHandler() http.Handler {
+	data := make(map[string][]byte)
 	router := httprouter.New()
 	router.GET("/", handleIndex)
 	router.GET("/help", handleHelp)
@@ -39,6 +59,18 @@ func createHandler() http.Handler {
 	router.GET("/json", handleJson)
 	router.GET("/json-trailers", handleJsonWithTrailers)
 	router.GET("/data", handleData)
+	router.POST("/database", func(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		handleDatabasePost(resp, req, data)
+	})
+	router.GET("/database/:key", func(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		handleDatabaseGet(resp, req, params, data)
+	})
+	router.PUT("/database/:key", func(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		handleDatabasePut(resp, req, params, data)
+	})
+	router.DELETE("/database/:key", func(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		handleDatabaseDelete(resp, req, params, data)
+	})
 	return router
 }
 
@@ -103,6 +135,71 @@ func handleData(resp http.ResponseWriter, req *http.Request, params httprouter.P
 	resp.Write(makeData(size))
 }
 
+func readJsonBody(resp http.ResponseWriter, req *http.Request) ([]byte, *databaseRecord, error) {
+	if !jsonContent.MatchString(req.Header.Get("Content-Type")) {
+		sendError(415, "Only JSON content is supported", resp)
+		return nil, nil, errors.New("Invalid content type")
+	}
+	body, _ := ioutil.ReadAll(req.Body)
+	var record databaseRecord
+	err := json.Unmarshal(body, &record)
+	if err != nil {
+		sendError(400, fmt.Sprintf("Invalid JSON: %s", err), resp)
+		return nil, nil, errors.New("Invalid JSON")
+	}
+	if record.Key == "" {
+		sendError(400, "Missing \"key\" property", resp)
+		return nil, nil, errors.New("Invalid content")
+	}
+	return body, &record, nil
+}
+
+func handleDatabasePost(resp http.ResponseWriter, req *http.Request, data map[string][]byte) {
+	body, record, err := readJsonBody(resp, req)
+	if err == nil {
+		data[record.Key] = body
+		resp.WriteHeader(201)
+	}
+}
+
+func handleDatabaseGet(resp http.ResponseWriter, req *http.Request, params httprouter.Params, data map[string][]byte) {
+	record := data[params.ByName("key")]
+	if record == nil {
+		sendError(404, "Not found", resp)
+	} else {
+		resp.Header().Set("Content-Type", "application/json")
+		resp.Header().Set("Content-Length", strconv.Itoa(len(record)))
+		resp.Write(record)
+	}
+}
+
+func handleDatabasePut(resp http.ResponseWriter, req *http.Request, params httprouter.Params, data map[string][]byte) {
+	key := params.ByName("key")
+	recordFound := data[key]
+	if recordFound == nil {
+		sendError(404, "Not found", resp)
+		return
+	}
+	body, newRecord, err := readJsonBody(resp, req)
+	if err == nil {
+		if newRecord.Key != key {
+			sendError(400, "Key does not match", resp)
+		} else {
+			data[newRecord.Key] = body
+		}
+	}
+}
+
+func handleDatabaseDelete(resp http.ResponseWriter, req *http.Request, params httprouter.Params, data map[string][]byte) {
+	key := params.ByName("key")
+	record := data[key]
+	if record == nil {
+		sendError(404, "Not found", resp)
+	} else {
+		delete(data, key)
+	}
+}
+
 func parseSize(req *http.Request, defaultSize int) int {
 	sizeStr := req.URL.Query().Get("size")
 	if sizeStr == "" {
@@ -124,4 +221,14 @@ func makeData(size int) []byte {
 		}
 	}
 	return buf.Bytes()
+}
+
+func sendError(statusCode int, message string, resp http.ResponseWriter) {
+	if message != "" {
+		resp.Header().Add("Content-Type", "text/plain")
+	}
+	resp.WriteHeader(statusCode)
+	if message != "" {
+		resp.Write([]byte(message))
+	}
 }
