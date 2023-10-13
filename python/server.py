bb5623cbfb85f8c52982928dcb06ecff6db396e5
Sampath Kumar <sampathm@google.com>
2023-10-13 17:13:05 +0200

    Update server.py
    Changes
    * Add Licence header
    * Clean-up the Title and descriptions
    * Reduce no.of functions
    * Add code comments

--- python/server.py
+++ python/server.py
@@ -1,64 +1,69 @@
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 # Header Manipulation - Example Server
-----
-Client sends a stream of requests and Server responds with a stream of responses
+
+Description: When a client sends a stream of gRPC requests, the server responds
+with a stream of responses. In this example, the server responds to the client
+with an example header to add to the request. The header will be named "hello"
+and will have the value "ext-proc".
 """
 from concurrent import futures
-import logging
 
+# [START serviceextension_add_header]
 import grpc
-
 import service_pb2
 import service_pb2_grpc
 from typing import Iterator
-from grpc import ServicerContext
-
-logger = logging.getLogger()
-logger.setLevel("INFO")
 
 
-def get_response():
-    header_mutation = service_pb2.HeaderMutation(
-        set_headers=list(
-            [
-                service_pb2.HeaderValueOption(
-                    header=service_pb2.HeaderValue(key="hello", value="ext-proc")
-                ),
-            ]
-        ),
-    )
-    request_headers = service_pb2.HeadersResponse(
-        response=service_pb2.CommonResponse(
-            header_mutation=header_mutation,
-        )
-    )
-    response = service_pb2.ProcessingResponse(request_headers=request_headers)
-    return response
-
-
-class TrafficExtensionCallout(service_pb2_grpc.ExternalProcessorServicer):
-    def ProcessSimple(self, request: service_pb2.ProcessingRequest, context):
-        print(request)
-        response = get_response()
-        return response
-
+class CalloutExample(service_pb2_grpc.ExternalProcessorServicer):
     def Process(
         self,
         request_iterator: Iterator[service_pb2.ProcessingRequest],
-        context: ServicerContext,
+        context: grpc.ServicerContext,
     ) -> Iterator[service_pb2.ProcessingResponse]:
         for request in request_iterator:
-            print(request)
-            response = get_response()
+            # TODO(Developer): Process the input request & prepare a response
+            # Add header `hello: ext-proc`
+            header_mutation = service_pb2.HeaderMutation(
+                set_headers=list(
+                    [
+                        service_pb2.HeaderValueOption(
+                            header=service_pb2.HeaderValue(
+                                key="hello", value="ext-proc"
+                            )
+                        )
+                    ]
+                )
+            )
+            response = service_pb2.ProcessingResponse(
+                request_headers=service_pb2.HeadersResponse(
+                    response=service_pb2.CommonResponse(
+                        status=service_pb2.CommonResponse.ResponseStatus.CONTINUE_AND_REPLACE,
+                        header_mutation=header_mutation,
+                    )
+                )
+            )
+            # Client is expected to send a stream of request
             yield response
 
 
-def serve():
-    port = "50051"
+# [END serviceextension_add_header]
+def serve(port="50051"):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-    service_pb2_grpc.add_ExternalProcessorServicer_to_server(
-        TrafficExtensionCallout(), server
-    )
+    service_pb2_grpc.add_ExternalProcessorServicer_to_server(CalloutExample(), server)
     server.add_insecure_port("[::]:" + port)
     server.start()
     print("Server started, listening on " + port)
@@ -66,5 +71,4 @@ def serve():
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
     serve()
